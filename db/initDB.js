1445b4943089aca82ca182fa92cca850992d753e
intouron <intouron@gmail.com>
2022-04-25 11:29:07 +0200

    arreglado hasta creacion de tablas en bd

--- db/initDB.js
+++ db/initDB.js
@@ -5,23 +5,39 @@ const { getConnection } = require('./db');
 async function main() {
   let connection;
 
+  console.log('main');
+
   try {
     connection = await getConnection();
+
+    console.log('borrando tablas existentes');
+    await connection.query('DROP TABLE IF EXISTS issues');
+    await connection.query('DROP TABLE IF EXISTS users');
+
     console.log('creando tablas');
     await connection.query(`
-        CREATE TABLE administrador (
-            id INTEGET PRIMARY KEY AUTO_INCREMENT, 
-            Título VARCHAR(255) NOT NULL,
-            Descripción VARCHAR(255) NOT NULL,
-            Foto LONGBLOB,
-            Ciudad VARCHAR(50) NOT NULL,
-            Barrio VARCHAR(50) NOT NULL,
-            PRIMARY KEY (id)
+        CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+            username VARCHAR(100) UNIQUE NOT NULL,
+            email VARCHAR(100) UNIQUE NOT NULL,
+            password VARCHAR(100) NOT NULL,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP          
+            )
+   `);
 
-        )
-   ` );
-   
-
+    await connection.query(`
+   CREATE TABLE issues (
+       id INTEGER PRIMARY KEY AUTO_INCREMENT, 
+       user_id INTEGER NOT NULL,
+       tittle VARCHAR(200) NOT NULL,
+       description VARCHAR(200) NOT NULL,
+       image VARCHAR(100) NOT NULL,
+       city VARCHAR(200) NOT NULL,
+       hood VARCHAR(200) NOT NULL,
+       created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+       FOREIGN KEY (user_id) REFERENCES users(id)        
+       )
+`);
   } catch (error) {
     console.error(error);
   } finally {
